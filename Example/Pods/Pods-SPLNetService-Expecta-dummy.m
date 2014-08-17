81530370ba76416fdc0223d5cca25e437280be00
Oliver Letterer <oliver.letterer@gmail.com>
2014-08-17 11:06:32 +0200

    Adds source.

---
+++ Example/Pods/Pods-SPLNetService-Expecta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPLNetService_Expecta : NSObject
+@end
+@implementation PodsDummy_Pods_SPLNetService_Expecta
+@end
---
+++ Example/Pods/Pods-SPLNetService-SPLNetService-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPLNetService_SPLNetService : NSObject
+@end
+@implementation PodsDummy_Pods_SPLNetService_SPLNetService
+@end
